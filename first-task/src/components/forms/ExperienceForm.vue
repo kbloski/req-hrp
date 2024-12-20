5e6a77a88fc7ac154ebf5f43f69f0058668e6069
Kamil Błoński <kblonski02@gmail.com>
2024-12-20 13:14:47 +0100

    Add ExperienceForm component

---
+++ first-task/src/components/forms/ExperienceForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container>
+        <v-form>
+            <v-text-field
+                label="Company"
+                variant="underlined"
+            ></v-text-field>
+            <v-text-field
+                label="Position"
+                variant="underlined"
+            ></v-text-field>
+            <v-text-field
+                label="Start date"
+                type="date"
+            ></v-text-field>
+            <v-text-field
+                label="End date"
+                type="date"
+            ></v-text-field>
+        </v-form>
+    </v-container>
+</template>
+
+<script setup>
+
+</script>
--- first-task/src/pages/index.vue
+++ first-task/src/pages/index.vue
@@ -4,13 +4,15 @@
     width="344"
     title="User From"
   >
-    <PersonalForm></PersonalForm>
-    <ContactForm></ContactForm>
+    <personal-form></personal-form>
+    <contact-form></contact-form>
+    <experience-for></experience-for>
   </v-card>
 </template>
 
 <script setup>
 import ContactForm from '@/components/forms/ContactForm.vue';
+import ExperienceForm from '@/components/forms/ExperienceForm.vue';
 import PersonalForm from '@/components/forms/PersonalForm.vue';
 
   //
