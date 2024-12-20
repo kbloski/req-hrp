fd1d2a8724b81bed38cbfe3647c9db10fee2c20d
Kamil Błoński <kblonski02@gmail.com>
2024-12-20 13:56:40 +0100

    Add personal form validate

--- first-task/src/components/forms/PersonalForm.vue
+++ first-task/src/components/forms/PersonalForm.vue
@@ -1,30 +1,81 @@
+<script setup>
+import { ref, reactive, watch, computed } from 'vue';
+
+const emits = defineEmits(['submit-form'])
+
+const name = ref(null)
+const surname = ref(null)
+const dateBrith = ref(null)
+
+// Errors
+const validationErrors = reactive({
+    name: null,
+    surname: null,
+    dateBrith: null
+})
+const isValidate = computed( 
+    () => 
+    !validationErrors.name 
+    && !validationErrors.surname 
+    && !validationErrors.dateBrith
+)
+
+watch([name, surname, dateBrith], ([name, surname, dateBrith]) => {
+    if (!name) validationErrors.name = 'This field is required'
+    else validationErrors.name = null
+
+    if (!surname) validationErrors.surname = 'This field is required'
+    else validationErrors.surname = null
+    
+    if (!dateBrith) validationErrors.dateBrith = 'This field is required'
+    else validationErrors.dateBrith = null
+}, { immediate: true})
+
+
+function onSubmit( event ){
+    event.preventDefault()
+    // if (!isValidate.value) return;
+
+    emits( 'submit-form', {
+        isValidate: isValidate.value,
+        formData: {
+            name: name.value,
+            surname: surname.value,
+            dateBrith: dateBrith.value
+        }
+    })
+}
+</script>
+
 <template>
     <v-container>
-        <v-form>
+        <v-form @submit="onSubmit">
             <v-text-field
                 v-model="name"
                 label="First name"
                 variant="underlined"
+                :rules="[() => validationErrors.name ?? true]"
+                required
             ></v-text-field>
             <v-text-field
                 v-model="surname"
                 label="Last name"
                 variant="underlined"
+                :rules="[() => validationErrors.surname ?? true]"
+                required
             ></v-text-field>
             <v-text-field
                 v-model="dateBrith"
                 color="primary"
                 type="date"
+                :rules="[() => validationErrors.dateBrith ?? true]"
+                required
             ></v-text-field>
+            <v-btn 
+                type="submit" 
+                color="success"
+                class="mt-3"
+            >Go next step</v-btn>
         </v-form>
     </v-container>
 </template>
-
-<script setup>
-import { ref } from 'vue';
-const name = ref(null)
-const surname = ref(null)
-const dateBrith = ref(null)
-
-
-</script>
