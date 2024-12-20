7b1a931179f764cff251800fdf06c161bdd1f007
Kamil Błoński <kblonski02@gmail.com>
2024-12-20 12:33:36 +0100

    Create vuexAPp

---
+++ first-task/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app class="rounded rounded-md">
+    <v-main class="d-flex align-center justify-center">
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
